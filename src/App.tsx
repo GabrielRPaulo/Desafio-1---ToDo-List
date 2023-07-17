92932c0076a16d84be41bc57307760b3a819bdd5
Gabriel Paulo <gabrielrodriguespaulo15@gmail.com>
2023-07-17 20:18:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+import "./global.css";
+import { Header } from "./components/Header";
+
+export function App() {
+
+  return (
+    <div>
+      <Header/>
+
+      <Post />
+    </div>
+  )
+}
+
+
